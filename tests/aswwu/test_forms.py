9def57fc4c128097c16d1ec0ed28a50f6846b8dd
Ethan Beaver <ethan.beaver@ahss.org>
2017-12-06 11:21:59 -0800

    Formatting changes to fix tests

--- tests/aswwu/test_forms.py
+++ tests/aswwu/test_forms.py
@@ -23,18 +23,18 @@ def test_view_job(testing_server, jobsdb_conn):
                     {"question": "High-School Attended", "id": 7},
                     {"question": "Current SM/ ACA?", "id": 10},
                     {"question": "If so, where?", "id": 11},
-                    {"question": "On average, how many credits will \
-                        you be taking?", "id": 12},
+                    {"question": "On average, how many credits will "\
+                        "you be taking?", "id": 12},
                     {"question": "How many hours do you hope to work?",
                         "id": 13},
-                    {"question": "Have you worked for ASWWU before? \
-                        If so, what job?", "id": 14},
-                    {"question": "What other jobs or responsibilities\
-                        will require your attention/ time?", "id": 15},
+                    {"question": "Have you worked for ASWWU before? "
+                        "If so, what job?", "id": 14},
+                    {"question": "What other jobs or responsibilities "\
+                        "will require your attention/ time?", "id": 15},
                     {"question": "Why do you want to work for ASWWU?",
                         "id": 16},
-                    {"question": "If there was one thing you could change\
-                        about ASWWU, what would it be?", "id": 17}
+                    {"question": "If there was one thing you could change "\
+                        "about ASWWU, what would it be?", "id": 17}
                 ],
                 "owner": "ryan.rabello",
                 "image": "",
